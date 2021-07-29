82242c25c2fa53e1787c0bb54519bd412e45660a
comn <gandi0330@naver.com>
2021-07-29 17:19:09 +0900

    baekjoon 20210729_2

---
+++ baekjoon/후위 표기식2.py
@@ -0,0 +1,28 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+strs = input()
+
+dict_ = {i : int(sys.stdin.readline())  for i in sorted(filter(lambda x : x.isalpha(), set(strs))) }
+print(dict_)
+
+stack_ = []
+
+for char in strs:
+    if char.isalpha() :
+        stack_.append(dict_[char])
+    else:
+        num2 = stack_.pop()
+        num1 = stack_.pop()
+
+        if char == '+' :
+            stack_.append(num1+num2)
+        elif char == '-' :
+            stack_.append(num1-num2)
+        elif char == '*' :
+            stack_.append(num1*num2)
+        elif char == '/' :
+            stack_.append(num1/num2)
+
+print('{:.2f}'.format(stack_.pop()))
